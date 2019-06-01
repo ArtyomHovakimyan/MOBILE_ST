b1b95af5d1d03181a74321dac9dd3078c438ba7d
User <artyom.hovakimyan01@gmail.com>
2019-06-01 23:56:20 +0400

    add model company

---
+++ Mic.Volo.MOBILE_ST/Data/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MOBILE_ST.Data.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public ICollection<Phone> Phones { get; set; }
+
+        public Company()
+        {
+            Phones = new Collection<Phone>();
+        }
+    }
+}
