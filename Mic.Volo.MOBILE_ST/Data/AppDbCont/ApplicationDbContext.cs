e35a41519e028e28e9626517ac67f279b53ab69d
User <artyom.hovakimyan01@gmail.com>
2019-06-02 00:09:11 +0400

    add folder and dbcontext

---
+++ Mic.Volo.MOBILE_ST/Data/AppDbCont/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Mic.Volo.MOBILE_ST.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MOBILE_ST.Data.AppDbCont
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Mic.Volo.MOBILE_ST/Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MOBILE_ST.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+
+        public int Qty { get; set; }
+
+        public int CakeId { get; set; }
+
+        public Phone Phone { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ShoppingCartId { get; set; }
+    }
+}
