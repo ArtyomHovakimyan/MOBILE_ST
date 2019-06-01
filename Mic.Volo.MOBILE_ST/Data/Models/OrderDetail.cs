3d0887653384961fefeae68ab85c9d6fe81916a2
User <artyom.hovakimyan01@gmail.com>
2019-06-02 00:00:02 +0400

    add model order

---
+++ Mic.Volo.MOBILE_ST/Data/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MOBILE_ST.Data.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string CakeName { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
