8a2e24185bb65358c4718bde1959621632b23bb3
User <artyom.hovakimyan01@gmail.com>
2019-06-01 23:53:58 +0400

    add model phone

---
+++ Mic.Volo.MOBILE_ST/Data/Models/Phone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MOBILE_ST.Data.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LongDescription { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public bool IsPhoneOfTheWeek { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ Mic.Volo.MOBILE_ST/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mic.Volo.MOBILE_ST.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
