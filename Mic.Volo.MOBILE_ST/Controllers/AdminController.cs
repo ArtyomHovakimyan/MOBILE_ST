2296791aa90743c864c4472cdf9564781596cc18
User <artyom.hovakimyan01@gmail.com>
2019-06-02 11:44:49 +0400

    add view

--- Mic.Volo.MOBILE_ST/Controllers/AdminController.cs
+++ Mic.Volo.MOBILE_ST/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Mic.Volo.MOBILE_ST.Data.Models;
 using Mic.Volo.MOBILE_ST.Data.Services;
 using Mic.Volo.MOBILE_ST.Data.Smod;
 using Mic.Volo.MOBILE_ST.Data.ViewModel;
@@ -24,11 +25,13 @@ namespace Mic.Volo.MOBILE_ST.Controllers
         public AdminController(
             IOrderService orderService,
             IMobSerice mobSerice,
+            IMapper mapper,
             IUWork uWork,
             ICompanyService companyService)
         {
             _orderService = orderService;
             _mobSerice = mobSerice;
+            _mapper = mapper;
             _uWork = uWork;
             _companyService = companyService;
             
@@ -56,22 +59,62 @@ namespace Mic.Volo.MOBILE_ST.Controllers
             }) ;
         }
         [HttpPost("add")]
-        //public async Task<IActionResult> AddPhone(PhoneSMod phoneSMod)
-        //{
-        //    if(!ModelState.IsValid)
-        //    {
-        //        var company = await _companyService.GetCompanies();
-        //        return View(new PhoneCreateUpdateViewModel
-        //        {
-        //            Companies = company,
-        //            PhoneSMod = phoneSMod
-        //        });
-        //    }
-        //    var phone=
-        //}
-        public IActionResult Index()
+        public async Task<IActionResult> AddPhone(PhoneSMod phoneSMod)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                var company = await _companyService.GetCompanies();
+                return View(new PhoneCreateUpdateViewModel
+                {
+                    Companies = company,
+                    PhoneSMod = phoneSMod
+                });
+            }
+            var phone = _mapper.Map<PhoneSMod, Phone>(phoneSMod);
+            await _mobSerice.AddPhoneAsync(phone);
+            await _uWork.CompleteAsync();
+            return RedirectToAction("ManagePhones");
+        }
+        [HttpGet("edit/{id}")]
+        public async Task<IActionResult> EditPhone(int id)
+        {
+            var phone = await _mobSerice.GetPhoneById(id);
+            var phoneSMod = _mapper.Map<Phone, PhoneSMod>(phone);
+            var company = await _companyService.GetCompanies();
+
+            return View(new PhoneCreateUpdateViewModel
+            {
+                Companies = company,
+                PhoneSMod = phoneSMod
+            });
+        }
+        [HttpPost("edit/{id}")]
+        public async Task<IActionResult> EditPhone(int id,[FromForm] PhoneSMod phoneSMod)
+        {
+            if(!ModelState.IsValid)
+            {
+                var company = await _companyService.GetCompanies();
+                return View(new PhoneCreateUpdateViewModel
+                {
+                    Companies = company,
+                    PhoneSMod = phoneSMod
+                });
+
+            }
+            var phone = _mapper.Map<PhoneSMod, Phone>(phoneSMod);
+            phone.Id = id;
+            _mobSerice.UpdatePhone(phone);
+            await _uWork.CompleteAsync();
+
+            return RedirectToAction("ManagePhones");
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhone(int id)
+        {
+            _mobSerice.Delete(id);
+            await _uWork.CompleteAsync();
+            return Ok();
         }
     }
 }
